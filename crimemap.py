db52a6a5f2f74c44c51e79def4840d0ce4cf4da4
joeleger <joeleger@users.noreply.github.com>
2020-05-12 18:19:20 -0400

    added a json dict to return information for crime place holder on mouseover

--- crimemap.py
+++ crimemap.py
@@ -1,5 +1,7 @@
 import dbconfig
+import json
 import os
+
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
@@ -12,15 +14,29 @@ DB = DBHelper()
 
 API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
 
+
 @app.route('/')
 def home():
     try:
         api_key = API_KEY
-        data = DB.get_all_inputs()
+        crimes = DB.get_all_crimes()
+        crimes = json.dumps(crimes)
+
     except Exception as e:
         print(e)
         data = None
-    return render_template("home.html", data=data, api_key=api_key)
+    return render_template("home.html", crimes=crimes, api_key=api_key)
+
+
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
+    return home()
 
 
 @app.route("/add", methods=["POST"])
